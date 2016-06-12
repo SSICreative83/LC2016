5d27b104c15b1d010b8eeec1f06fe64b66c10e24
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2016-06-12 00:01:50 -0700

    Create D2_AddBinary.java

---
+++ D2_AddBinary.java
@@ -0,0 +1,65 @@
+public class Solution {
+    public String addBinary(String a, String b) {
+        if(a == null || a.length() == 0) {
+            return b;
+        }
+        if(b == null || b.length() == 0) {
+            return a;
+        }
+        
+        char[] arra = a.toCharArray();
+        char[] arrb = b.toCharArray();
+        int i = arra.length - 1;
+        int j = arrb.length - 1;
+        int reslen = i > j ? i + 2 : j + 2;
+        int[] res = new int[reslen];
+        int k = reslen - 1;
+        int plus = 0;
+
+        while(i >= 0 || j >= 0 || plus > 0) {
+            if(i < 0 && j < 0) {
+                res[k] = plus;
+            } else if(i < 0) {
+                res[k] = (arrb[j] - '0') + plus;
+                j--;
+            } else if(j < 0) {
+                res[k] = (arra[i] - '0') + plus;
+                i--;
+            } else {
+                res[k] = arra[i] - '0' + arrb[j] - '0' + plus;
+                i--;
+                j--;
+            }
+            plus = 0;
+            
+            if(res[k] == 2) {
+                res[k] = 0;
+                plus = 1;
+            } else if(res[k] == 3) {
+                res[k] = 1;
+                plus = 1;
+            }
+            k--;
+        }
+        
+        
+        boolean isZero = true;
+        int h = 0;
+        for(; h < reslen; h++) {
+            if(res[h] != 0) {
+                isZero = false;
+                break;
+            }
+        }
+        
+        if(isZero) {
+            return "0";
+        }
+        
+        StringBuilder resb = new StringBuilder();
+        for(; h < reslen; h++) {
+            resb.append(res[h]);
+        }
+        return resb.toString();
+    }
+}
