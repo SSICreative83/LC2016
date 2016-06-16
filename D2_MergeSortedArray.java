8c6a21e1757daee34e425d176410341b7afb5a44
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2016-06-15 23:16:17 -0700

    Create D2_MergeSortedArray.java

---
+++ D2_MergeSortedArray.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0) return;
+        
+        for(int i = m - 1, j = n - 1, k = m + n - 1; i >= 0 || j >= 0; k--) {
+            if(i < 0) {
+                nums1[k] = nums2[j--];
+            } else if(j < 0) {
+                nums1[k] = nums1[i--];
+            } else if(nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i--];
+            } else if(nums1[i] <= nums2[j]) {
+                nums1[k] = nums2[j--];
+            }
+        }
+    }
+}
