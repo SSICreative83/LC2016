3f0f92ced2c8b765e4036ed01bccdb47fb6f8961
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2016-08-21 23:36:56 -0700

    Update D2_PascalsTriangle.java

--- D2_PascalsTriangle.java
+++ D2_PascalsTriangle.java
@@ -8,19 +8,13 @@ public class Solution {
         res.add(list1);
         if(numRows == 1) return res;
         
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(1);
-        list2.add(1);
-        res.add(list2);
-        if(numRows == 2) return res;
-        
         List<Integer> last = new ArrayList<>();
-        for(int i = 3; i <= numRows; i++) {
+        for(int i = 1; i < numRows; i++) {
             List<Integer> listi = new ArrayList<>();
             listi.add(1);
             
-            last = res.get(i - 2);
-            for(int j = 1; j < i - 1; j++) {    //Need to use i as boundry
+            last = res.get(i - 1);
+            for(int j = 1; j < i; j++) {    //Need to use i as boundry
                 listi.add(j, last.get(j - 1) + last.get(j));
             }
             
