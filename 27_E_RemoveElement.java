ef2fd1c75d0dcea56a6cd8ced04bb4a2f56a3d49
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2016-08-27 21:32:09 -0700

    Create 27_E_RemoveElement.java

---
+++ 27_E_RemoveElement.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0)    return 0;
+        
+        int i = 0, j = nums.length - 1;
+        while( i < j ) {
+            if(nums[i] != val) {
+                i++;
+                continue;
+            }
+            if(nums[j] == val) {
+                j--;
+                continue;
+            }
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+        
+        if(nums[i] == val) {
+            return i;
+        } else {
+            return i + 1;
+        }
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = tmp;
+    }
+}
