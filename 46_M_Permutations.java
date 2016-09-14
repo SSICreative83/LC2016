ad5306c9aafc83ba3814e263a69ef5d4fca0d6b2
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2016-09-13 22:28:52 -0700

    Update 46_M_Permutations.java

--- 46_M_Permutations.java
+++ 46_M_Permutations.java
@@ -1,27 +1,65 @@
 public class Solution {
-    public List<List<Integer>> permute(int[] nums) {
+    //recursion, DFS
+    public List<List<Integer>> permute1(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums.length == 0)    return res;
+        if(nums == null || nums.length == 0)    return res;
         
+        Arrays.sort(nums);
         List<Integer> tmp = new ArrayList<>();
-        dfs(nums, tmp, res);
+        List<Integer> nnums = new ArrayList<>();
+        for(int i : nums) {
+            nnums.add(i);
+        }
+        dfs(nnums, tmp, res);
         return res;
     }
     
-    public void dfs(int[] nums, List<Integer> tmp, List<List<Integer>> res) {
-        if(tmp.size() == nums.length) {
+    public void dfs(List<Integer> nums, List<Integer> tmp, List<List<Integer>> res) {
+        if(nums.size() == 0) {
             res.add(tmp);
             return;
         }
         
-        for(int i = 0; i < nums.length; i++) {
-            int cur = nums[i];
-            if(tmp.contains(cur)) {
-                continue;
-            }
+        for(int i = 0; i < nums.size(); i++) {
+            int cur = nums.get(i);
+            List<Integer> nnums = new ArrayList<>(nums);
+            nnums.remove(i);
             List<Integer> ntmp = new ArrayList<>(tmp);
             ntmp.add(cur);
-            dfs(nums, ntmp, res);
+            dfs(nnums, ntmp, res);
         }
+    }
+    
+    //Iterative
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0)    return res;
+        if(nums.length == 1) {
+            List<Integer> tmp = new ArrayList<>();
+            tmp.add(nums[0]);
+            res.add(tmp);
+            return res;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            int cur = nums[i];
+            
+            int[] nnums = new int[nums.length - 1];
+            for(int j = 0; j < nums.length; j++) {
+                if(j == i)  continue;
+                if(j < i) {
+                    nnums[j] = nums[j];
+                } else {
+                    nnums[j - 1] = nums[j];
+                }
+            }
+            
+            List<List<Integer>> cres = permute(nnums);
+            for(List<Integer> clist : cres) {
+                clist.add(cur);
+                res.add(clist);
+            }
+        }
+        return res;
     }
 }
