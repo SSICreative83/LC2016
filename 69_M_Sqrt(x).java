b01cdb44b051a3d0ddad8b583e47f0b7d21d8f9a
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2016-09-19 21:21:54 -0700

    Update 69_M_Sqrt(x).java

--- 69_M_Sqrt(x).java
+++ 69_M_Sqrt(x).java
@@ -19,4 +19,23 @@ public class Solution {
         
         return res;
     }
+    
+    //jiuzhang
+    public int sqrt(int x) {
+        // find the last number which square of it <= x
+        long start = 1, end = x;
+        while (start + 1 < end) {
+            long mid = start + (end - start) / 2;
+            if (mid * mid <= x) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (end * end <= x) {
+            return (int) end;
+        }
+        return (int) start;
+    }
 }
