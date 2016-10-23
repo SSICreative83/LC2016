b4d32c323d7381c80f8fd77228645b66a94db24e
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2016-10-22 20:32:53 -0700

    Create 169_E_MajorityElement.java

---
+++ 169_E_MajorityElement.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        for(int i = 0; i < n; i++) {
+            int cur = nums[i];
+            if(map.containsKey(cur)) {
+                int curCt = map.get(cur);
+                if(curCt >= n / 2) {
+                    return cur;
+                } else {
+                    map.put(cur, map.get(cur) + 1);
+                }
+            } else {
+                map.put(cur, 1);
+            }
+        }
+        return 0;
+    }
+}
