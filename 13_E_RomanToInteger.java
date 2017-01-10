942d7af905e9047fa4cd23679ef18d4a683d28f5
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-10 09:31:37 -0800

    Update 13_E_RomanToInteger.java

--- 13_E_RomanToInteger.java
+++ 13_E_RomanToInteger.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public int romanToInt(String s) {
+    public int romanToInt1(String s) {
         
         //define a hashtable for Roman chars
         HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
@@ -23,4 +23,21 @@ public class Solution {
 		}
         return sum;
     }
+	
+	private Map<Character, Integer> map =
+		new HashMap<Character, Integer>() {{
+		put('I', 1); put('V', 5); put('X', 10);
+		put('L', 50); put('C', 100); put('D', 500);
+		put('M', 1000);
+	}};
+
+	public int romanToInt(String s) {
+		int prev = 0, total = 0;
+		for (char c : s.toCharArray()) {
+			int curr = map.get(c);
+			total += (curr > prev) ? (curr - 2 * prev) : curr;
+			prev = curr;
+		}
+		return total;
+	}
 }
