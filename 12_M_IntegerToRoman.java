57df083b59a0d24b98c147e653b3c6dff56cf24c
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-10 09:27:57 -0800

    Update 12_M_IntegerToRoman.java

--- 12_M_IntegerToRoman.java
+++ 12_M_IntegerToRoman.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public String intToRoman(int num) {
+    public String intToRoman1(int num) {
         String result = "";
         
         String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
@@ -23,5 +23,33 @@ public class Solution {
             result += ones[num];
         }
         return result;
+    }
+    
+    private static final int[] values = {
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    };
+    
+    private static final String[] symbols = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    };
+    
+    public String intToRoman(int num) {
+        StringBuilder roman = new StringBuilder();
+        int i = 0;
+        while (num > 0) {
+            int k = num / values[i];
+            for (int j = 0; j < k; j++) {
+                roman.append(symbols[i]);
+                num -= values[i];
+            }
+            i++;
+        }
+        return roman.toString();
     }
 }
