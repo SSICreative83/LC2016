0d4d4d2c003385213865e56eeb240de554949ce6
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-11 09:19:17 -0800

    Update and rename 110_D1_BalancedBinaryTree.java to 110_E_BalancedBinaryTree.java

--- 110_E_BalancedBinaryTree.java
+++ 110_E_BalancedBinaryTree.java
@@ -8,7 +8,8 @@
  * }
  */
 public class Solution {
-    public boolean isBalanced(TreeNode root) {
+    //top down recursion
+    public boolean isBalanced1(TreeNode root) {
         if(root == null) {
             return true;
         }
@@ -24,5 +25,19 @@ public class Solution {
         }
         
         return Math.max(height(root.left), height(root.right)) + 1;
+    }
+    
+    //bottom up recursion, use -1 to represent un-banlanced
+    public boolean isBalanced(TreeNode root) {
+        return maxDepth(root) != -1;
+    }
+    
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int L = maxDepth(root.left);
+        if (L == -1) return -1;
+        int R = maxDepth(root.right);
+        if (R == -1) return -1;
+        return (Math.abs(L - R) <= 1) ? (Math.max(L, R) + 1) : -1;
     }
 }
