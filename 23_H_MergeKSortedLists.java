c23df9c07fda8386b16b85fc2fb14e4fd79c4291
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-12 09:53:34 -0800

    Update 23_H_MergeKSortedLists.java

--- 23_H_MergeKSortedLists.java
+++ 23_H_MergeKSortedLists.java
@@ -6,6 +6,12 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+ 
+class ListNodeComparator implements Comparator<ListNode>{
+    public int compare(ListNode l1, ListNode l2) {
+        return l1.val - l2.val;
+    }
+} 
 public class Solution {
     //Solution1: merge one by one, time limit exceeded
     public ListNode mergeKLists1(ListNode[] lists) {
@@ -21,7 +27,7 @@ public class Solution {
     }
     
     //Solution2: divide and conquer
-    public ListNode mergeKLists(ListNode[] lists) {
+    public ListNode mergeKLists2(ListNode[] lists) {
         if(lists == null || lists.length == 0)  return null;
         ListNode res = mergeHelper(lists, 0, lists.length - 1);
         return res;
@@ -59,4 +65,29 @@ public class Solution {
         
         return dummy.next;
     }
+    
+    //solution3: heap
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0)  return null;
+        
+        PriorityQueue<ListNode> heap = new PriorityQueue<>(lists.length, new ListNodeComparator());
+        for(int i = 0; i < lists.length; i++) {
+            if(lists[i] != null) {
+                heap.offer(lists[i]);
+            }
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while(!heap.isEmpty()) {
+            ListNode min = heap.poll();
+            p.next = min;
+            p = p.next;
+            if(min.next != null) {
+                heap.offer(min.next);
+            }
+        }
+        
+        return dummy.next;
+    }      
 }
