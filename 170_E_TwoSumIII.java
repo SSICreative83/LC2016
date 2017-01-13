8012eb870dfec43b6de6db80e32a081e922757ea
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-12 22:16:09 -0800

    Create 170_E_TwoSumIII.java

---
+++ 170_E_TwoSumIII.java
@@ -0,0 +1,34 @@
+public class TwoSum { 
+    Map<Integer, Integer> map = new HashMap<>();
+
+    // Add the number to an internal data structure.
+	public void add(int number) {
+	    if(map.containsKey(number)) {
+	        map.put(number, map.get(number) + 1);
+	    } else {
+	        map.put(number, 1);
+	    }
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    for(Integer key : map.keySet()) {
+	        int target = value - key;
+	        if(key == target)  {
+	            if(map.get(key) >= 2) {
+	                return true;
+	            }
+	        }
+	        else if(map.containsKey(target)) {
+	             return true;
+	        }
+	    }
+	    return false;
+	}
+}
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
