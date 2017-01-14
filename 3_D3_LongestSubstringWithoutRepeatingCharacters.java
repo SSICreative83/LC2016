a9de108e28fc3cfdc02e1591f439449a99ff5bda
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-13 22:51:11 -0800

    Update 3_D3_LongestSubstringWithoutRepeatingCharacters.java

--- 3_D3_LongestSubstringWithoutRepeatingCharacters.java
+++ 3_D3_LongestSubstringWithoutRepeatingCharacters.java
@@ -23,8 +23,7 @@ public class Solution {
         return max;
     }
     
-    //O(n)
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring2(String s) {
         if(s == null || s.length() == 0)    return 0;
         char[] arr = s.toCharArray();
         HashMap<Character, Integer> map = new HashMap<>();
@@ -46,5 +45,35 @@ public class Solution {
             }
         }
         return max;
+    }
+    
+    //using a boolean[] instead of map
+    public int lengthOfLongestSubstring3(String s) {
+        boolean[] exist = new boolean[256];
+        int i = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j++) {
+            while (exist[s.charAt(j)]) {
+                exist[s.charAt(i)] = false;
+                i++;
+            }
+            exist[s.charAt(j)] = true;
+            maxLen = Math.max(j - i + 1, maxLen);
+        }
+        return maxLen;
+    }
+    
+    //use an array to map char to index
+    public int lengthOfLongestSubstring(String s) {
+        int[] charMap = new int[256];
+        Arrays.fill(charMap, -1);
+        int i = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (charMap[s.charAt(j)] >= i) {
+                i = charMap[s.charAt(j)] + 1;
+            }
+            charMap[s.charAt(j)] = j;
+            maxLen = Math.max(j - i + 1, maxLen);
+        }
+        return maxLen;
     }
 }
