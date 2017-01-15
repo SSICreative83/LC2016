6b21e46f5be90c44e84c7d690e8ddbc72aa851d1
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-15 11:43:52 -0800

    Update 78_M_Subsets.java

--- 78_M_Subsets.java
+++ 78_M_Subsets.java
@@ -1,5 +1,6 @@
 public class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+    //DFS, recursion
+    public List<List<Integer>> subsets1(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if(nums == null || nums.length == 0)   return res;
         
@@ -22,5 +23,28 @@ public class Solution {
             dfs(i + 1, nums, ntmp, res);
         }
         
+    }
+    
+    //Iterative
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0)   return res;
+        
+        Arrays.sort(nums);
+        List<Integer> empty = new ArrayList<>();
+        res.add(empty);
+        
+        for(int i = 0; i < nums.length; i++) {
+            int cur = nums[i];
+            int curResSize = res.size();
+            for(int j = 0; j < curResSize; j++) {
+                List<Integer> curList = res.get(j);
+                List<Integer> newList = new ArrayList<>(curList);
+                newList.add(cur);
+                res.add(newList);
+            } 
+        }
+        
+        return res;        
     }
 }
