8921d66b243f6b70355e056a21ab1a6f06e17caf
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-15 19:17:17 -0800

    Update 162_M_FindPeakElement.java

--- 162_M_FindPeakElement.java
+++ 162_M_FindPeakElement.java
@@ -1,5 +1,6 @@
 public class Solution {
-    public int findPeakElement(int[] nums) {
+    //O(n)
+    public int findPeakElement1(int[] nums) {
         
         int len = nums.length;
         if(len == 0)    return -1;
@@ -17,6 +18,40 @@ public class Solution {
             } else if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
                 return i;
             }
+        }
+        
+        return -1;
+    }
+    
+    //O(logn)
+    public int findPeakElement(int[] nums) {
+        
+        int len = nums.length;
+        if(len == 0)    return -1;
+        if(len == 1)    return 0;
+        if(nums[0] > nums[1])   return 0;
+        if(nums[len - 1] > nums[len - 2])   return len - 1;
+        
+        int start = 1;
+        int end = len - 2;
+        
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if(nums[mid] < nums[mid - 1]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+            
+        }
+        
+        if(nums[start] > nums[start + 1] && nums[start] > nums[start - 1]) {
+            return start;
+        } else if(nums[end] > nums[end + 1] && nums[end] > nums[end - 1]) {
+            return end;
         }
         
         return -1;
