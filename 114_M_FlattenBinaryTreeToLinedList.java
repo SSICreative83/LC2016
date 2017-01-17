bfd82d504f2d30788589fec0d9366837c73f3d23
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-01-17 09:38:38 -0800

    Update 114_M_FlattenBinaryTreeToLinedList.java

--- 114_M_FlattenBinaryTreeToLinedList.java
+++ 114_M_FlattenBinaryTreeToLinedList.java
@@ -34,3 +34,41 @@ public class Solution {
         return left;
     }
 }
+
+//re-do by myself
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        flattenHelper(root);
+    }
+    
+    public void flattenHelper(TreeNode root) {
+        if(root == null)    return;
+        if(root.left == null && root.right == null)     return;
+        
+        flattenHelper(root.left);
+        flattenHelper(root.right);
+        
+        connect(root, root.left, root.right);
+    }
+    
+    public void connect(TreeNode root, TreeNode left, TreeNode right) {
+        if(root == null || left == null)    return;
+        TreeNode p = left;
+        while(p != null && p.right != null) {
+            p = p.right;
+        }
+        
+        p.right = right;
+        root.right = left;
+        root.left = null;
+    }
+}
