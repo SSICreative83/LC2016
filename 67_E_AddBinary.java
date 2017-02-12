302014ca58ae9eaff269bd08ac50d1af9af0fe7a
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-02-11 19:31:45 -0800

    Update and rename 67_D2_AddBinary.java to 67_E_AddBinary.java

--- 67_E_AddBinary.java
+++ 67_E_AddBinary.java
@@ -62,4 +62,31 @@ public class Solution {
         }
         return resb.toString();
     }
+    
+    // shorter solution
+    public String addBinary(String a, String b) {
+        if(a == null || b == null) {
+            return "0";
+        }
+        
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i >= 0 || j >= 0) {
+            int sum = carry;
+            if(i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if(j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if(carry != 0) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
 }
