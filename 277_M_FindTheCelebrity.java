7f638bc36bbd3f10bce0adf2536427cbaf244f1f
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-02-12 13:32:44 -0800

    Update 277_M_FindTheCelebrity.java

--- 277_M_FindTheCelebrity.java
+++ 277_M_FindTheCelebrity.java
@@ -2,7 +2,7 @@
       boolean knows(int a, int b); */
 
 public class Solution extends Relation {
-    public int findCelebrity(int n) {
+    public int findCelebrity1(int n) {
         
         //find the column where all values are 1
         
@@ -28,5 +28,18 @@ public class Solution extends Relation {
             }
         }
         return -1;
+    }
+    
+    // smart way of selecting candidate
+    public int findCelebrity(int n) {
+        int candidate = 0;
+        for(int i = 1; i < n; i++){
+            if(knows(candidate, i))
+                candidate = i;
+        }
+        for(int i = 0; i < n; i++){
+            if(i != candidate && (knows(candidate, i) || !knows(i, candidate))) return -1;
+        }
+        return candidate;
     }
 }
