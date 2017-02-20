6f7bd652f43749f649474c48a5ca385542b8a33e
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-02-20 15:05:36 -0800

    Update 93_M_RestoreIPAddress.java

--- 93_M_RestoreIPAddress.java
+++ 93_M_RestoreIPAddress.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public List<String> restoreIpAddresses(String s) {
+    public List<String> restoreIpAddresses1(String s) {
         List<String> res = new ArrayList<>();
         if(s == null || s.length() < 4 || s.length() > 12)    return res;
         
@@ -30,5 +30,52 @@ public class Solution {
         if(s.charAt(0) == '0')  return s.equals("0");
         int val = Integer.parseInt(s);
         return val > 0 && val <= 255;
+    }
+    
+   //DFS done by self. Key part is to keep a count 
+   public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        if(s == null || s.length() == 0) {
+            return res;
+        }
+        dfs(s, 0, s.length(), "", res, 0);
+        return res;
+    }
+    
+    public void dfs(String s, int start, int end, String tmp, List<String> res, int count) {
+        if(count == 3 && isValid(s.substring(start, end))) {
+            res.add(tmp + "." + s.substring(start, end));
+            return;
+        } else if(count == 3) {
+            return;
+        }
+        
+        
+        for(int i = start + 1; i < end; i++) {
+            String cur = s.substring(start, i);
+            if(isValid(cur)) {
+                int newCnt = count + 1;
+                String ntmp = tmp + "." + cur;
+                if(tmp.equals("")) {
+                    ntmp = ntmp.substring(1);
+                }
+                dfs(s, i, end, ntmp, res, newCnt);
+            }
+        }
+        
+    }
+    
+    public boolean isValid(String s) {
+        if(s == null || s.length() == 0) {
+            return false;
+        }
+        if(s.length() > 1 && s.charAt(0) == '0') {
+            return false;
+        }
+        if(s.length() > 3) {
+            return false;
+        }
+        int num = Integer.valueOf(s);
+        return num <= 255 && num >= 0;
     }
 }
