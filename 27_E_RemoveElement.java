9c5509a530a4da686e144a096332ce8b3e622106
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-02-22 20:35:50 -0800

    Update 27_E_RemoveElement.java

--- 27_E_RemoveElement.java
+++ 27_E_RemoveElement.java
@@ -29,4 +29,30 @@ public class Solution {
         nums[j] = nums[i];
         nums[i] = tmp;
     }
+    
+    //done by self
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0)    return 0;
+        
+        int i = 0, j = nums.length - 1;
+        for(; i < j; ) {
+
+            if(nums[j] == val ) {
+                j--;
+                continue;
+            }
+            if(nums[i] != val) {
+                i++;
+                continue;
+            }
+            
+            swap(nums, i, j);
+        }
+    
+        if(nums[i] == val) {
+            return i;
+        } else {
+            return i + 1;
+        }
+    }
 }
