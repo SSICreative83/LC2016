230e940b1c89e684f2a8ee33581fd47e24ef6ea2
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-02-23 00:28:27 -0800

    Update 325_M_MaximumSizeSubarraySum.java

--- 325_M_MaximumSizeSubarraySum.java
+++ 325_M_MaximumSizeSubarraySum.java
@@ -1,4 +1,26 @@
 public class Solution {
+    //clean solution using one loop
+   public int maxSubArrayLen(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int max = 0;
+        int sum=0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(sum==k){
+                max = Math.max(max, i+1);
+            }
+            int diff = sum-k;
+            if(map.containsKey(diff)){
+                max = Math.max(max, i-map.get(diff));
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
+        }
+        return max;
+    }
+    
+    
     public int maxSubArrayLen(int[] nums, int k) {
         if(nums == null || nums.length == 0) {
             return 0;
