583aeb9d8f15d6bea75cfe2e573e05bb8dea00cf
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-19 15:56:44 -0700

    Update and rename 300_M_Longest_Increasing_Subsequence.java to 300_M_LongestIncreasingSubsequence.java

--- 300_M_LongestIncreasingSubsequence.java
+++ 300_M_LongestIncreasingSubsequence.java
@@ -20,6 +20,7 @@ public class Solution {
         return res;
     }
     
+    // DP
     public int lengthOfLIS(int[] nums) {
         if(nums == null || nums.length == 0) {
             return 0;
@@ -37,6 +38,26 @@ public class Solution {
                     max = dp[i];
                 }
             }
+        }
+        return max;
+    }
+    
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int max = 1;
+        for(int i = 1; i < nums.length; i++) {
+            dp[i] = 1;      //Important, need to initialize each value to be 1
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
         }
         return max;
     }
