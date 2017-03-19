8aefa953c9a7f415073978a370b74289b32992ae
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-19 13:27:45 -0700

    Update 89_M_GrayCode.java

--- 89_M_GrayCode.java
+++ 89_M_GrayCode.java
@@ -8,4 +8,27 @@ public class Solution {
         }
         return res;   
     }
+    
+    //Iterative solution 
+    // 0, 1 -> 00, 01, 11, 10 -> 000, 001, 011, 010, 110, 111, 101, 100
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        if(n == 0) {
+            return res;
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int j = 0; j < res.size(); j++) {
+                tmp.add(res.get(j));
+            }
+            for(int j = res.size() - 1; j >= 0; j--) {
+                tmp.add((1 << (i - 1)) + res.get(j));
+            }
+            
+            res = tmp;
+        }
+        return res;
+    }
 }
