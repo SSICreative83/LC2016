803f60d6f435cc62a954dd1404ab8dc3e40f9993
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-23 10:34:15 -0700

    Update 53_E_MaximumSubarray.java

--- 53_E_MaximumSubarray.java
+++ 53_E_MaximumSubarray.java
@@ -1,4 +1,26 @@
 public class Solution {
+    //self
+     public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int maxEndingHere = nums[0];
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            int cur = nums[i];
+            if(maxEndingHere > 0) {
+                maxEndingHere += cur;
+            } else {
+                maxEndingHere = cur;
+            }
+            
+            res = Math.max(res, maxEndingHere);
+        }
+        
+        return res;
+    }
+    
     public int maxSubArray(int[] nums) {
         if(nums == null || nums.length == 0) {
             return 0;
