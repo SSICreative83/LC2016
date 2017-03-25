1d7d526fd21bbfb9022c9a4856155c6972637a14
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-24 22:37:23 -0700

    Update 39_M_CombinationSum.java

--- 39_M_CombinationSum.java
+++ 39_M_CombinationSum.java
@@ -4,11 +4,11 @@ public class Solution {
         if(candidates == null || candidates.length == 0)    return res;
         List<Integer> tmp = new ArrayList<>();
         Arrays.sort(candidates);
-        dfs(candidates, target, tmp, res);
+        dfs(candidates, 0, target, tmp, res);
         return res;
     }
     
-    public void dfs(int[] candidates, int target, List<Integer> tmp, List<List<Integer>> res) {
+    public void dfs(int[] candidates, int start, int target, List<Integer> tmp, List<List<Integer>> res) {
         int curSum = 0;
         for(int i : tmp) {
             curSum += i;
@@ -20,18 +20,12 @@ public class Solution {
         if(curSum > target) {
             return;
         } 
-        
-        int curMax = Integer.MIN_VALUE;
-        if(tmp.size() > 0) {
-            curMax = tmp.get(tmp.size() - 1);
-        }
-        for(int i = 0; i < candidates.length; i++) {
+
+        for(int i = start; i < candidates.length; i++) {
             int cur = candidates[i];
-            if(cur < curMax) continue;  //increasing order
-            List<Integer> ntmp = new ArrayList<>();
-            ntmp.addAll(tmp);
+            List<Integer> ntmp = new ArrayList<>(tmp);
             ntmp.add(cur);
-            dfs(candidates, target, ntmp, res);
+            dfs(candidates, i, target, ntmp, res);
         }
     }
 }
