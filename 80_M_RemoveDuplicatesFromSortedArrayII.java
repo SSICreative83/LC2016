240eba62c011c6a0730cfbf5e6e52f4d0ccdbfb7
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-25 01:01:43 -0700

    Update 80_M_RemoveDuplicatesFromSortedArrayII.java

--- 80_M_RemoveDuplicatesFromSortedArrayII.java
+++ 80_M_RemoveDuplicatesFromSortedArrayII.java
@@ -13,4 +13,18 @@ public class Solution {
         }
         return i;
     }
+    
+    //self
+    public int removeDuplicates(int[] nums) {
+        if(nums.length <= 2)    return nums.length;
+        
+        int i = 2, j = i;
+        for(; j < nums.length; j++) {
+            if(nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
+            } 
+        }
+        return i;
+    }
 }
