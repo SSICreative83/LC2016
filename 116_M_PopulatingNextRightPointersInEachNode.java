4c87a680d61c5208fcbcc5957a0da8d74706ae7a
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-25 13:38:05 -0700

    Update 116_M_PopulatingNextRightPointersInEachNode.java

--- 116_M_PopulatingNextRightPointersInEachNode.java
+++ 116_M_PopulatingNextRightPointersInEachNode.java
@@ -7,6 +7,29 @@
  * }
  */
 public class Solution {
+    
+    //self, pass only root node
+   public void connect(TreeLinkNode root) {
+        if(root == null)    return;
+        helper(root);
+    }
+    
+    public void helper(TreeLinkNode root) {
+        if(root == null)    return;
+        if(root.left == null || root.right == null) {
+            return;
+        }
+        
+        root.left.next = root.right;
+        if(root.next != null) {
+            root.right.next = root.next.left;
+        }
+        
+        helper(root.left);
+        helper(root.right);
+    }    
+    
+    //another solution, pass left and right, same logic as above
     public void connect(TreeLinkNode root) {
         if(root == null)    return;
         helper(root, root.left, root.right);
