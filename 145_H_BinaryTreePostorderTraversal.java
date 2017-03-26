1896044f59e318ee472a31ffd59cf1712dfb38cc
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-25 21:44:54 -0700

    Update 145_H_BinaryTreePostorderTraversal.java

--- 145_H_BinaryTreePostorderTraversal.java
+++ 145_H_BinaryTreePostorderTraversal.java
@@ -8,6 +8,39 @@
  * }
  */
 public class Solution {
+    //self
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) {
+            return res ;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+
+        while(p != null) {
+            stack.push(p); 
+            res.add(p.val);
+            p = p.right;
+        }
+        
+        while(!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if(cur.left != null) {
+                p = cur.left;
+                while(p != null) {
+                    stack.push(p);
+                    res.add(p.val);
+                    p = p.right;
+                }
+            }
+        }
+
+        Collections.reverse(res);
+        return res;
+    }
+    
+    //more concise solution using 1 while loop
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if(root == null) {
