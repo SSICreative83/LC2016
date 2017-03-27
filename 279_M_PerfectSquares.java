589fb989165ff34f8e60ffcd4c356ba4cb09e93d
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-27 01:24:48 -0700

    Update 279_M_PerfectSquares.java

--- 279_M_PerfectSquares.java
+++ 279_M_PerfectSquares.java
@@ -1,5 +1,49 @@
 public class Solution {
+    //DP optimal
+    public int numSquares(int n) {
+        if(n <= 0) {
+            return -1;
+        }
+        
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for(int i = 0; i * i <= n; ++i)
+            dp[i * i] = 1;
 
+        for (int i = 0; i <= n; ++i)
+            for (int j = 0; i + j * j <= n; ++j)    //need to update dp[i + j * j]
+                dp[i + j * j] = Math.min(dp[i] + 1, dp[i + j * j]);
+
+        return dp[n];
+       
+    }    
+
+    //DP non-optimal TLE
+    public int numSquares(int n) {
+        if(n <= 0) {
+            return -1;
+        }
+        
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        for(int i = 1; i * i <= n; i++) {
+            dp[i * i] = 1;
+        }
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j < i; j++) {
+                if(dp[j] + dp[i - j] < dp[i]) {
+                    dp[i] = dp[j] + dp[i - j];
+                }
+            }
+        }
+
+        return dp[n];
+       
+    }
+    
+    
     // Time limit exceeded
     // Try one by one from big to small
     public int numSquares(int n) {
