99fffc123db7c3cc8aa39bb876c075552bc9e5f5
Joyce Zhang <zhanghuijun1988@users.noreply.github.com>
2017-03-27 19:26:56 -0700

    Create 280_M_WiggleSort.java

---
+++ 280_M_WiggleSort.java
@@ -0,0 +1,16 @@
+public class Solution {
+    
+     public void wiggleSort(int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            if( (i%2==1 && nums[i] < nums[i-1]) || (i%2==0 && nums[i] > nums[i-1]) ) {
+                swap(nums, i-1, i);
+            }
+        }
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
